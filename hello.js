fefcc918a4e950659652a6306774290c0d4c9aa8
Laia Galvez Bach <101476844+laiashelley@users.noreply.github.com>
2025-08-11 12:11:41 +0200

    Add hello.js file to blablabla branch

---
+++ hello.js
@@ -0,0 +1,7 @@
+console.log("Hello from the blablabla branch!");
+
+function greet(name) {
+    return `Hello, ${name}! This file was created in the blablabla branch.`;
+}
+
+module.exports = { greet };
